505c20c5e4fa3268c1b57775d2691433bbc5cf63
BinkaArushankina <albinarushankina@gmail.com>
2023-07-31 16:45:03 +0200

    Homework2 update

--- src/Homework2.sql
+++ src/Homework2.sql
@@ -4,18 +4,27 @@ SELECT ProductName, Price * 1.1 AS Price_USD FROM OrderDetails JOIN Products
     ON OrderDetails.ProductID=Products.ProductID ORDER BY Price DESC LIMIT 1
 
 2.)Вывести два самых дорогих товара из категории Beverages из USA
-SELECT ProductName, Price FROM Products
+SELECT * FROM Products
+JOIN Categories ON Products.CategoryID=Categories.CategoryID
+JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+WHERE Categories.CategoryName LIKE 'beverages'
+AND Suppliers.Country LIKE 'usa'
+ORDER BY Products.Price DESC LIMIT 2
+
+
+//-------------------------------------------------------------------------------------------------//
+SELECT * FROM Products
 WHERE CategoryID = (SELECT CategoryID FROM Categories WHERE CategoryName = 'Beverages')
   AND ProductID IN (SELECT ProductID FROM OrderDetails WHERE OrderID IN
                    (SELECT OrderID FROM Orders WHERE CustomerID IN
                    (SELECT CustomerID FROM Customers WHERE Country = 'USA')))
 ORDER BY Price DESC LIMIT 2;
-
+//------------------------------------------------------------------------------------------------//
 3.)Удалить товары с ценой менее 5 EUR
 DELETE FROM Products WHERE Price <=5
 
 4.)Вывести список стран, которые поставляют морепродукты
-SELECT Country FROM Suppliers WHERE SupplierID IN
+SELECT DISTINCT Suppliers.Country FROM Suppliers WHERE SupplierID IN
 (SELECT SupplierID FROM Products WHERE CategoryID IN
 (SELECT CategoryID FROM Categories WHERE CategoryName='Seafood'))
 
